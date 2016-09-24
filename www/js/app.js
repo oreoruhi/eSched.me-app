fb4df687dc989928024fe8a90fb30fb45616e95a
Rugie Barrameda <norugie015@hotmail.com>
2016-09-24 18:30:33 +0800

    Template changes
    template changes

--- www/js/app.js
+++ www/js/app.js
@@ -15,7 +15,7 @@ angular.module('eSchedMe', ['ionic', 'backand', 'eSchedMe.controllers', 'eSchedM
 
             }
             
-            cordova.plugins.Keyboard.disableScroll(true)
+            //cordova.plugins.Keyboard.disableScroll(true)
 
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
