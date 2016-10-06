e60e7d904b7decc233a71e620d9d6d698fa5ca69
dj-diaz <denniel.joshua@gmail.com>
2016-10-07 00:49:42 +0800

    finish project date constraint

---
+++ www/js/services/CordovaPluginService.js
@@ -0,0 +1,34 @@
+(function () {
+  'use strict';
+
+  angular.module('eSchedMe')
+    .factory('CordovaPlugins', cordovaServiceFunction);
+
+  cordovaServiceFunction.$inject = ['$ionicPlatform', '$cordovaDatePicker'];
+
+  function cordovaServiceFunction($ionicPlatform, $cordovaDatePicker) {
+    return {
+
+    };
+
+    function openDatePicker (project, provider) {
+      $ionicPlatform.ready(function() {
+        if (provider === 'project') {
+          var projectOptions = {
+            date: new Date(),
+            mode: 'date',
+            minDate: new Date().valueOf()
+          };
+          return $cordovaDatePicker.show(projectOptions);
+        }
+        var options = {
+          date: new Date(),
+          mode: 'date', // or 'time'
+          minDate: new Date(project.start).valueOf(),
+          maxDate: new Date(project.end).valueOf()
+        };
+        return $cordovaDatePicker.show(options);
+      });
+    }
+  }
+})();
