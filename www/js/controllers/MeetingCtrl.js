38e539ab7eb636812fda7ac2363593ab092b82b6
Rugie Barrameda <norugie015@hotmail.com>
2016-12-27 00:02:16 +0800

    creating meeting

--- www/js/controllers/MeetingCtrl.js
+++ www/js/controllers/MeetingCtrl.js
@@ -7,7 +7,8 @@ angular.module('eSchedMe.controllers')
     $ionicPopover, 
     $ionicModal, 
     $ionicPlatform, 
-    $cordovaDatePicker){
+    $cordovaDatePicker,
+    $stateParams){
     var vm = this;
 
     function init(){
@@ -28,6 +29,7 @@ angular.module('eSchedMe.controllers')
         .fromTemplateUrl('templates/modals/meeting/create-meeting.html', function (modal) {
             $scope.openDatePicker = vm.openDatePicker;
             $scope.closeModal = vm.closeModal;
+            $scope.saveMeeting = vm.saveMeeting;
             vm.modal = modal;
             vm.modal.show();
     }, {
@@ -58,6 +60,41 @@ angular.module('eSchedMe.controllers')
         vm.meetings = MeetingData.get();
         console.log(vm.meetings);
       };
+
+    var geocoder = new google.maps.Geocoder();
+
+
+
+      vm.saveMeeting = function(location, date, status) {
+        console.log(location);
+        geocoder.geocode( {'address': location}, function(results, status) {
+        if (status == 'OK') {
+            console.log(results[0].geometry.location.lat() + " " + results[0].geometry.location.lng());
+            var data = {
+                "activity_id": $stateParams.project.id,
+                "location": location,
+                "long": results[0].geometry.location.lng(),
+                "lat": results[0].geometry.location.lat(),
+                "status": status,
+                "date": date,
+            };
+            console.log(data);
+
+            MeetingData.save(data,
+            function(resp, header) {
+                console.log(resp);
+                vm.getMeetings();
+                vm.closeModal();
+            },
+            function(error) {
+                console.log(error);
+            });
+        } else {
+            console.log('Geocode was not successful for the following reason: ' + status);
+        }
+        });
+        console.log(location);
+      }
 
     init();
 
