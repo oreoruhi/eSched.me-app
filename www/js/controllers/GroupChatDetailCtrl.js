7ae551f28d0bed2973baef34e9da49130cead1af
DJ Diaz <denniel.joshua@gmail.com>
2017-03-04 20:12:24 +0800

    add most of group chat functionality

--- www/js/controllers/GroupChatDetailCtrl.js
+++ www/js/controllers/GroupChatDetailCtrl.js
@@ -21,11 +21,26 @@
 
     function _init() {
       $log.info(requests);
+      self.user_id = window.localStorage.getItem('user_id');
+      $log.info(self.user_id);
       self.data = requests.data;
+      setTimeout(function() {$ionicScrollDelegate.scrollBottom(true);}, 500);
+      Pusher.subscribe('groupchat.' + self.data[0].id, 'App\\Events\\GroupChatEvent', function(message) {
+        $log.info(message);
+        self.data.push(message.message);
+        $ionicScrollDelegate.scrollBottom(true);
+      });
     }
 
-    self.sendMessage = function(id, message) {
-
+    self.sendGroupMessage = function(message) {
+      var id = self.data[0].id;
+      $log.info('ID is: ' + id);
+      $http.post(API.URL + '/api/v1/group/message', {
+        'group_chat_id': id,
+        'message': message
+      }).then(function(data) {
+        $log.info(data);
+      });
     };
 
     _init();
