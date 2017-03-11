122f8ed4fdacdfd238b0c1d4e08d0576d4f37204
Jackbard <fcktardjoe@gmail.com>
2017-03-11 17:17:30 +0800

    Update Tag and Space in Chat

--- www/js/controllers/TagsCtrl.js
+++ www/js/controllers/TagsCtrl.js
@@ -17,9 +17,11 @@
       $log.warn('Tags Controller Initializing...');
       $http.get(API.URL + '/api/v1/me/pending_activity_tags')
         .then(function (resp) {
-          $log.debug(resp);
+          $log.info(resp);
           self.pendingTags = resp.data;
         });
+        self.me = JSON.parse(window.localStorage.getItem('user'));
+        $log.debug(self.me);
     }
 
     self.acceptTag = function (activityId) {
@@ -27,16 +29,29 @@
       $ionicLoading.show({template: '<ion-spinner>'});
       $http.post(API.URL + '/api/v1/me/pending_activity_tags/approve/' + activityId)
         .then(function () {
-          DataService.GetUserById()
-            .then(function(result) {
-              self.user = result.data.data;
-              window.localStorage.setItem('user_id', self.user.id);
-              window.localStorage.setItem('user', JSON.stringify(self.user));
-              $ionicLoading.hide();
-            });
+          // DataService.GetUserById()
+          //   .then(function(result) {
+          //     self.user = result.data.data;
+          //     window.localStorage.setItem('user_id', self.user.id);
+          //     window.localStorage.setItem('user', JSON.stringify(self.user));
+          //     $ionicLoading.hide();
+          //   });
+          $ionicLoading.hide();
           _init();
         });
     };
+
+    self.untagMe = function(activityId) {
+      $log.warn("Untagging me on Project ID: " + activityId);
+      $log.warn("My user id is: " + self.me.id);
+      $ionicLoading.show({template: '<ion-spinner>'});
+      $http.post(API.URL + '/api/v1/me/pending_activity_tags/decline/' + activityId)
+      .then(function(result) {
+        $log.info(result);
+        $ionicLoading.hide();
+        _init();
+      });
+    }
 
   });
 })();
